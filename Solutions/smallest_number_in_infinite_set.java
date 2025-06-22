02862f039d9b9e725feac0ac2eb095a2c4e5116c
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-06-22 13:14:59 +0530

    Finding the smallest

---
+++ Solutions/smallest_number_in_infinite_set.java
@@ -0,0 +1,24 @@
+class SmallestInfiniteSet {
+    PriorityQueue<Integer> p;
+    public SmallestInfiniteSet() {
+        p= new PriorityQueue<>();
+        for (int i = 1; i <= 1000; i++) {
+         p.add(i);
+}
+
+    }
+    
+   public int popSmallest() {
+    
+    return p.poll();
+}
+
+    
+    public void addBack(int num) {
+        if(p.contains(num))
+        System.out.print("null");
+        else
+        p.add(num);
+    }
+}
+
