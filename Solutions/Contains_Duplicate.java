f96b3ada28c727517f9dc1ba13e1f3939aaf6869
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-08-24 15:26:15 +0530

    Contains duplicate

--- Solutions/Contains_Duplicate.java
+++ Solutions/Contains_Duplicate.java
@@ -2,10 +2,11 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
     Set<Integer> ss = new HashSet<>();
         for(int i:nums){
-            ss.add(i);
+        if(ss.contains(i)){
+            return true;
         }
-        if(ss.size()<nums.length)
-        return true;
+        ss.add(i);
+        }
         return false;
     }
 }
