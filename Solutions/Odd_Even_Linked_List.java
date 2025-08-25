fbe234531a7407708d636bb5d17c4c36564b4a3a
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-08-25 22:51:13 +0530

    Odd Even Linked List

--- Solutions/Odd_Even_Linked_List.java
+++ Solutions/Odd_Even_Linked_List.java
@@ -10,31 +10,19 @@
  */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-       ListNode dummy = new ListNode(0);
-       ListNode curr = dummy;
-       ListNode node = head;
-       ListNode node2 = head;
-       int i=1;
-       int j=1;
-       while(node!=null){
-        if(i%2!=0){
-            curr.next = new ListNode(node.val);
-            curr = curr.next;
+        if(head==null){
+            return null;
         }
-        i++;
-        node = node.next;
-       } 
-        while(node2!=null){
-        if(j%2==0){
-            curr.next = new ListNode(node2.val);
-            curr = curr.next;
-        }
-        j++;
-        node2 = node2.next;
-       } 
-
-
-   return dummy.next;
-
+     ListNode even = head.next;
+     ListNode odd = head;
+     ListNode markeven = even;
+     while(even!=null && even.next!=null){
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+     }
+     odd.next = markeven;
+     return head;
     }
 }
