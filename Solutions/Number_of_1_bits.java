403c2304138a28c52946992b61074b3b2ff6a3d2
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-12-21 00:32:27 +0530

    No. of 1 bits

---
+++ Solutions/Number_of_1_bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingWeight(int n) {
+       int count=1;
+       while(n!=1){
+        if(n%2!=0)
+        count++;
+        n/=2;
+       } 
+       return count;
+    }
+}
