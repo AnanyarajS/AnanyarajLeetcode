9483e62ef6f53477e36f412ca9713f882edb050d
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2026-07-08 13:49:05 +0530

    Group Anagrams

---
+++ Solutions/Group_Anagrams.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> list = new ArrayList<>();
+        HashMap<String, List<String>> map = new HashMap<>();
+        for(String s : strs){
+            char [] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String str = new String(arr);
+            if(!map.containsKey(str)){
+                map.put(str, new ArrayList<>());
+            }
+            map.get(str).add(s);
+        }
+        for (Map.Entry<String, List<String>> entry : map.entrySet()){
+            list.add(entry.getValue());
+        }
+        return list;
+    }
+}
