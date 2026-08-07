41ffcf0200c89e9f277f0dead6a6b7bbf8dfc588
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2026-08-07 23:22:40 +0530

    Fruit into Baskets

---
+++ Solutions/Fruit_Into_Baskets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int l = 0;
+        int r = 0;
+        int max = 0;
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        while(r<fruits.length){
+            hm.put(fruits[r], hm.getOrDefault(fruits[r], 0)+1);
+            while(hm.size()>2 && l<=r){
+                hm.put(fruits[l], hm.get(fruits[l])-1);
+                if(hm.get(fruits[l])==0) hm.remove(fruits[l]);
+                l++;
+            } 
+            max = Math.max(max, r-l+1);     
+            r++;                              
+        }
+        if(hm.size()>2) max--;
+        return max;
+    }
+}
