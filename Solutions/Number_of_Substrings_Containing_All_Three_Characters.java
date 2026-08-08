71bc455164226ed62ede28482d1b3584a1c3faef
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2026-08-08 13:08:27 +0530

    Substrings with abc

---
+++ Solutions/Number_of_Substrings_Containing_All_Three_Characters.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int [] freq = new int[3];
+        int l = 0, r = 0;
+        int count = 0;
+        while(r<s.length()){
+            freq[s.charAt(r)-'a']++;
+            while(freq[0]>0 && freq[1]>0 && freq[2]>0){
+                count+= s.length()-r;
+                freq[s.charAt(l)-'a']--;
+                l++;
+            }
+            r++;
+        }
+        return count;
+
+        
+    }
+}
