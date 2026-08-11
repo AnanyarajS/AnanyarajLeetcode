7a83de1a29fc02f37b327693721094690b7a55c2
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2026-08-11 23:37:25 +0530

    Next Greater Element I

---
+++ Solutions/Next_Greater_Element_i.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for(int i=nums2.length-1;i>=0;i--){
+            if(st.isEmpty()){
+                hm.put(nums2[i], -1);
+                st.push(nums2[i]);
+                continue;
+            }
+            while(!st.isEmpty() && st.peek()<=nums2[i]){
+                st.pop();
+            }
+
+            if(!st.isEmpty())
+            hm.put(nums2[i], st.peek());
+            else hm.put(nums2[i], -1);
+            st.push(nums2[i]);
+        }
+        int [] result = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            result[i] = hm.get(nums1[i]);
+        }
+        return result;
+
+
+        
+        }
+}
